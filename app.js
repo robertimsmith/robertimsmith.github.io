730aed880fe564f2d4481166bc96ea71e7b289f7
robertimsmith <robertmsmith@blueyonder.co.uk>
2020-10-27 15:01:21 +0000

    adding a script

---
+++ app.js
@@ -0,0 +1,11 @@
+var minutes = 1000 * 60;
+var hours = minutes * 60;
+var days = hours * 24;
+var years = days * 365;
+var t = Date.now();
+
+var y = Math.round(t / years);
+
+console.log(y);
+var name="Robert";
+//document.getElementById("output").innerHTML=name;
