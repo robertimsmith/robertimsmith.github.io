e8d1ccacd82c9e9d8f0f04edc8c43ed5b91158fd
robertimsmith <robertmsmith@blueyonder.co.uk>
2020-10-29 16:08:16 +0000

    created folder structure

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 //create a server object on http://localhost:8080/
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'content-type': 'text/html' })
-    fs.createReadStream('index.html').pipe(res)
+    fs.createReadStream('html/index.html').pipe(res)
   })
   
   server.listen(process.env.PORT || 8080)
