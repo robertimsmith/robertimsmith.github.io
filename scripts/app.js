fedff58049668e2e89fb7f5a3509669ff027766b
robertimsmith <robertmsmith@blueyonder.co.uk>
2020-10-30 12:47:50 +0000

    adding external link

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,14 @@
-const http = require('http');
-const fs = require('fs');
-
 //create a server object on http://localhost:8080/
-const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    fs.createReadStream('index.html').pipe(res)
-  })
-  
-  server.listen(process.env.PORT || 8080)
+var express = require('express')
+var app = express()
+
+app.use(express.static('/Users/robertsmith/Documents/GitHub/robertimsmith.github.io'));
+
+var server = app.listen(8080, function () {
+
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log('Express app listening at http://%s:%s', host, port)
+
+})
